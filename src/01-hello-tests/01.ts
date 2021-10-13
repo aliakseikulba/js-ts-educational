b1c3cbe1a953c8fdba2e4987742d903631dd4122
aliakseikulba <kulba.av@gmail.com>
2021-10-13 10:53:28 +0300

    test lesson completed

---
+++ src/01-hello-tests/01.ts
@@ -0,0 +1,18 @@
+export function sum(a: number, b: number) {
+  return a + b;
+}
+
+export function multiply(a: number, b: number) {
+  return a * b;
+}
+
+
+export function splitIntoWords(sentence: string) {
+    const words = sentence.toLowerCase().split(' ');
+
+     return words.filter(word => word !== '' && word !== '-' )
+      .map(word => word
+        .replace('!', '')
+        .replace('.', '')
+        .replace(',', ''))
+}
