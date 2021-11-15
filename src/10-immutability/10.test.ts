e54ab1a5b2eb320973afe35ee6db43ed5e78a6a7
aliakseikulba <kulba.av@gmail.com>
2021-11-15 14:53:57 +0300

    test is done

--- src/10-immutability/10.test.ts
+++ src/10-immutability/10.test.ts
@@ -7,4 +7,20 @@ function makeHaircut(u: UserType, volume: number) {
   };
   return copy;
 }
-);
+
+test('reference type test', () => {
+ let user: UserType = {
+   name: 'Bob',
+   hair: 100,
+   address: {
+     title: 'Minsk'
+   }
+  }
+
+  const client = makeHaircut(user, 2);
+
+ expect(user.hair).toBe(100);
+ expect(client.hair).toBe(50);
+ expect(client.address).toBe(user.address)
+
+});
